2af8f4dc86d89ffef8b821085d8886d4194bd4a7
Thais Danieli <78539909+thaisdanieli@users.noreply.github.com>
2023-09-26 13:52:48 -0300

    Delete exercise02.sql

--- exercise02.sql
+++
@@ -1,8 +0,0 @@
-create table produto(
-	codigo_Produto int not null,
-    descricao_Produto varchar(30),
-    preco_Produto float
-)default charset utf8;
-
-ALTER TABLE produto
-ADD PRIMARY KEY (codigo_Produto);

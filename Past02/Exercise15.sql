36fb7a68229d6c338d215e39a8808d8ba1687169
Thais Danieli <78539909+thaisdanieli@users.noreply.github.com>
2023-10-05 14:07:32 -0300

    Update Exercise15.sql

--- Past02/Exercise15.sql
+++ Past02/Exercise15.sql
@@ -6,3 +6,18 @@ SUM(valor * 1.15) AS Valor_Atualizado
 FROM duplicata
 WHERE year(vencimento) = 2016 AND datediff(now(), vencimento) > 0
 GROUP BY Vencimento; 
+
+--------------------
+
+SELECT NOME, date_format(vencimento,' %d/%m/%y') AS Vencimento,
+concat('R$', FORMAT(valor*1.15,2)) AS Valor_Atualizado,
+concat('R$', valor) AS Valor_Vencido
+FROM duplicata
+WHERE vencimento BETWEEN '2016-01-01' AND '2016-12-31';
+
+SELECT NOME, date_format(vencimento,' %d/%m/%y') AS Vencimento,
+concat('R$', FORMAT(valor*1.15,2)) AS Valor_Atualizado,
+concat('R$', valor) AS Valor_Vencido,
+datediff(curdate(), vencimento) AS Dias_Vencidos
+FROM duplicata
+WHERE YEAR(vencimento) = '2016' AND datediff(curdate(), vencimento) > 0;
